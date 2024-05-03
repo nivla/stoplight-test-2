62eff07a154c4577d553105d10a18f56c5059ba2
Alvin Dickson <dickson.alvin@gmail.com>
2024-05-03 17:51:01 -0400

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NTE1Nw/spectral.js?branch_slug=master&token=b53ca1f7-3fcc-42b1-991d-2ba57c539da7";
+export default { extends: ruleset };
